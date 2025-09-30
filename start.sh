a628a130e905b00c1b9e3250db18774e486b333a
Vishal Solanki <vishal.remarkadab@gmail.com>
2025-09-30 17:34:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
-
 #!/bin/bash
-pip install --upgrade pip# Install Tesseract OCR
+# Upgrade pip first
+pip install --upgrade pip
+
+# Install Tesseract OCR
 apt-get update
 apt-get install -y tesseract-ocr
 
-# Start Flask app using gunicorn
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Download SpaCy English model
+python -m spacy download en_core_web_sm
+
+# Start Flask app
 gunicorn app:app --bind 0.0.0.0:$PORT
